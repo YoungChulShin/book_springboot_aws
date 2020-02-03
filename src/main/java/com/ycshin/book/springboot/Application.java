d659c94562eb70de9e45f981630b9c781751dab0
YoungChulShin <go1323@gmail.com>
2020-02-03 21:36:14 +0900

    Lombok 및 테스트코드 추가

---
+++ src/main/java/com/ycshin/book/springboot/Application.java
@@ -0,0 +1,11 @@
+package com.ycshin.book.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication  // 스프링 부트 자동설정, 빈 읽기 및 생성을 자동으로 설정
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args); // 내장 WAS를 실행
+    }
+}
---
+++ src/test/java/com/ycshin/book/springboot/web/HelloControllerTest.java
@@ -0,0 +1,29 @@
+package com.ycshin.book.springboot.web;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@RunWith(SpringRunner.class)    // 테스트를 진행할 때 JUnit에 내장된 runner외에 다른 것을 실행하도록 해준다
+@WebMvcTest
+public class HelloControllerTest {
+
+    @Autowired
+    private MockMvc mvc;
+
+    @Test
+    public void hello가_리턴된다() throws Exception {
+        String hello = "hello";
+
+        mvc.perform(get("/hello"))
+                .andExpect(status().isOk())
+                .andExpect(content().string(hello));
+    }
+}
