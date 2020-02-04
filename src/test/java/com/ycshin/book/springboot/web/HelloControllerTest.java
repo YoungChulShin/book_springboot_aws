2e504b953d51c382b0d4cc6c1fb0ee0babc78dac
YoungChulShin <go1323@gmail.com>
2020-02-05 05:47:59 +0900

    2장 완성

--- src/test/java/com/ycshin/book/springboot/web/HelloControllerTest.java
+++ src/test/java/com/ycshin/book/springboot/web/HelloControllerTest.java
@@ -7,9 +7,12 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
+import static org.hamcrest.Matchers.is;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+
 
 @RunWith(SpringRunner.class)    // 테스트를 진행할 때 JUnit에 내장된 runner외에 다른 것을 실행하도록 해준다
 @WebMvcTest
@@ -25,5 +28,17 @@ public class HelloControllerTest {
         mvc.perform(get("/hello"))
                 .andExpect(status().isOk())
                 .andExpect(content().string(hello));
+    }
+
+    public void helloDto가_리턴된다() throws  Exception {
+        String name = "hello";
+        int amount = 1000;
+
+        mvc.perform(get("/hello/dto")
+                        .param("name", name)
+                        .param("amount", String.valueOf(amount)))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.name", is(name)))
+                .andExpect(jsonPath("$.amount", is(amount)));
     }
 }
