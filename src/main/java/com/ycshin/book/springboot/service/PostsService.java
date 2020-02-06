12dafce1658dc234ce3a64bbdb26205bfbdb4ca7
YoungChulShin <go1323@gmail.com>
2020-02-06 23:32:01 +0900

    3장 1/2

---
+++ src/main/java/com/ycshin/book/springboot/service/PostsService.java
@@ -0,0 +1,20 @@
+package com.ycshin.book.springboot.service;
+
+import com.ycshin.book.springboot.domain.posts.PostsRepository;
+import com.ycshin.book.springboot.web.dto.PostsSaveRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+
+@RequiredArgsConstructor
+@Service
+public class PostsService {
+
+    private final PostsRepository postsRepository;
+
+    @Transactional
+    public Long save(PostsSaveRequestDto requestDto) {
+        return  postsRepository.save(requestDto.toEntity()).getId();
+    }
+}
