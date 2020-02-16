e6be74fd13afa0cf5bef28190c131d67c20c14a8
YoungChulShin <go1323@gmail.com>
2020-02-16 11:36:46 +0900

    5장. 구글 연동 기능 추가

---
+++ src/main/java/com/ycshin/book/springboot/config/auth/LoginUserArgumentResolver.java
@@ -0,0 +1,35 @@
+package com.ycshin.book.springboot.config.auth;
+
+import com.ycshin.book.springboot.config.auth.dto.SessionUser;
+import lombok.RequiredArgsConstructor;
+import org.springframework.core.MethodParameter;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.support.WebDataBinderFactory;
+import org.springframework.web.context.request.NativeWebRequest;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.method.support.ModelAndViewContainer;
+
+import javax.servlet.http.HttpSession;
+
+@RequiredArgsConstructor
+@Component
+public class LoginUserArgumentResolver implements HandlerMethodArgumentResolver {
+
+    private HttpSession httpSession;
+
+    // 객체가 올바른 타입인지 확인
+    @Override
+    public boolean supportsParameter(MethodParameter parameter) {
+
+        boolean isLoginUserAnnotation = parameter.getParameterAnnotation(LoginUser.class) != null;
+        boolean isUserClass = SessionUser.class.equals(parameter.getParameterType());
+
+        return isLoginUserAnnotation && isUserClass;
+    }
+
+    // 파라미터에 전달할 객체
+    @Override
+    public Object resolveArgument(MethodParameter parameter, ModelAndViewContainer mavContainer, NativeWebRequest webRequest, WebDataBinderFactory binderFactory) throws Exception {
+        return httpSession.getAttribute("userName");
+    }
+}
